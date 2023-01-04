d4feeb5308af954049b86589c0f3009e354fbb5b
Shushant Patel <97536200+shushantpatel644@users.noreply.github.com>
2023-01-04 17:50:57 +0530

    Python

--- Day 5/main.py
+++ Day 5/main.py
@@ -5,3 +5,12 @@ print("hey I am a good bou \n and this viewer is a good boy")
 # hey Shushant, Please don't remove this line
 # Author: Shushant
 # Course: 100DaysOfCode
+#  comments 
+# print("hey i am shushant patel /n and this viewer is a good boy")
+# print("hello world")
+print("This is a print statatement")
+print("hello world !!! ") #printing hello world
+
+'''
+hii i am shsuahnt patel
+'''
